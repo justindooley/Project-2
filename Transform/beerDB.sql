19f213580cc8e82af456106e69b7b12505a2e783
justindooley <justindooley5@gmail.com>
2018-12-18 19:52:50 -0500

    queries

--- Transform/beerDB.sql
+++ Transform/beerDB.sql
@@ -1,10 +1,16 @@
-create database beerDB;
-use beerDB;
-select * from beer_data;
+CREATE DATABASE beer_db;
+USE beer_db;
 
-ALTER TABLE beer_data1
+ALTER TABLE beer_data
+MODIFY COLUMN ID VARCHAR(100);
+
+ALTER TABLE beer_data
 ADD PRIMARY KEY (ID);
 
+SELECT * FROM beer_data;
 
-ALTER TABLE beer_data1
-MODIFY COLUMN ID VARCHAR(100);
+CREATE DATABASE beer_review_db;
+
+USE beer_review_db;
+
+SELECT * FROM beer_review;
